00bdd682d2af1abae9d358cf35c36fdf0072735d
vansterhant <erik.karrsgard@gmail.com>
2023-11-28 08:53:30 +0100

    Create dotnet.yml

--- Addon.Optimizely.GraphQL/TypeDefinitions/ContentAreaType.cs
+++ Addon.Optimizely.GraphQL/TypeDefinitions/ContentAreaType.cs
@@ -12,7 +12,7 @@ public class ContentAreaType : ObjectType<ContentArea>
         
         descriptor.Field(t => t.FilteredItems)
             .Type<ListType<ContentInterfaceType>>()
-            .ResolveWith<Resolvers>(r => r.GetItemsForContentArea(default, default));
+            .ResolveWith<Resolvers>(r => r.GetItemsForContentArea(default!, default!));
 
         descriptor.Field(t => t.Tag).Type<StringType>();
     }
--- Addon.Optimizely.GraphQL/TypeDefinitions/PageDataTypeBase.cs
+++ Addon.Optimizely.GraphQL/TypeDefinitions/PageDataTypeBase.cs
@@ -45,76 +45,12 @@ public abstract class PageDataTypeBase<T> : ObjectType<T> where T : PageData
         descriptor.Field(t => t.Saved).Type<DateType>();
         
         descriptor.Field("_parent").Type<PageDataInterfaceType>()
-            .ResolveWith<Resolvers>(r => r.GetParent(default, default));
+            .ResolveWith<Resolvers>(r => r.GetParent(default!, default!));
         
         descriptor.Field("_ancestors").Type<ListType<PageDataInterfaceType>>()
-            .ResolveWith<Resolvers>(r => r.GetAncestors(default, default));
+            .ResolveWith<Resolvers>(r => r.GetAncestors(default!, default!));
 
         descriptor.Implements<PageDataInterfaceType>();
-    }
-}
-
-public abstract class PageDataFilterTypeBase<T> : FilterInputType<T> where T : PageData
-{
-    protected override void Configure(IFilterInputTypeDescriptor<T> descriptor)
-    {
-        base.Configure(descriptor);
-
-        descriptor.BindFields(BindingBehavior.Explicit);
-
-        descriptor.Field(t => t.VisibleInMenu);
-        
-        descriptor.Field(t => t.Name);
-        descriptor.Field(t => t.ContentLink);
-        descriptor.Field(t => t.ParentLink);
-        descriptor.Field(t => t.ContentGuid);
-        descriptor.Field(t => t.IsDeleted);
-
-        descriptor.Field(t => t.Status);
-        descriptor.Field(t => t.IsPendingPublish);
-        descriptor.Field(t => t.StartPublish);
-        descriptor.Field(t => t.StopPublish);
-
-        descriptor.Field(t => t.Language);
-
-        descriptor.Field(t => t.ExistingLanguages);
-        descriptor.Field(t => t.MasterLanguage);
-        
-        descriptor.Field(t => t.Created);
-        descriptor.Field(t => t.CreatedBy);
-        descriptor.Field(t => t.Changed);
-        descriptor.Field(t => t.ChangedBy);
-        descriptor.Field(t => t.Deleted);
-        descriptor.Field(t => t.DeletedBy);
-        descriptor.Field(t => t.Saved);
-    }
-}
-
-public abstract class PageDataSortTypeBase<T> : SortInputType<T> where T : PageData
-{
-    protected override void Configure(ISortInputTypeDescriptor<T> descriptor)
-    {
-        base.Configure(descriptor);
-
-        descriptor.BindFields(BindingBehavior.Explicit);
-
-        descriptor.Field(t => t.VisibleInMenu);
-
-        descriptor.Field(t => t.Status);
-        descriptor.Field(t => t.IsPendingPublish);
-        descriptor.Field(t => t.StartPublish);
-        descriptor.Field(t => t.StopPublish);
-
-        descriptor.Field(t => t.Name);
-        descriptor.Field(t => t.IsDeleted);
-
-        descriptor.Field(t => t.Created);
-        descriptor.Field(t => t.CreatedBy);
-        descriptor.Field(t => t.Changed);
-        descriptor.Field(t => t.ChangedBy);
-        descriptor.Field(t => t.Deleted);
-        descriptor.Field(t => t.DeletedBy);
-        descriptor.Field(t => t.Saved);
     }
 }
 
